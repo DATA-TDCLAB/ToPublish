a814189c3b3d70d77ee9d0d82ea0eb81d47a2db8
unknown <juanita.ortizz@tdclab.com>
2023-05-09 11:23:58 -0500

    No port

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
+//APS_CLIENT_ID="DGZWJQI4QRVhOCFOMTUfAFjG7DloabU9"
+//APS_CLIENT_SECRET="waG7axH7kfQOVZTe"
+//APS_CALLBACK_URL="http://localhost:10000/api/auth/callback"
+//SERVER_SESSION_SECRET="custom-encryption-phrase"
+//PORT=10000
+
+
 let { APS_CLIENT_ID, APS_CLIENT_SECRET, APS_CALLBACK_URL, SERVER_SESSION_SECRET, PORT } = process.env;
 if (!APS_CLIENT_ID || !APS_CLIENT_SECRET || !APS_CALLBACK_URL || !SERVER_SESSION_SECRET) {
     console.warn('Missing some of the environment variables.');
@@ -5,7 +12,9 @@ if (!APS_CLIENT_ID || !APS_CLIENT_SECRET || !APS_CALLBACK_URL || !SERVER_SESSION
 }
 const INTERNAL_TOKEN_SCOPES = ['data:read'];
 const PUBLIC_TOKEN_SCOPES = ['viewables:read'];
-PORT = PORT || 10000;
+PORT = PORT;
+
+
 
 module.exports = {
     APS_CLIENT_ID,
